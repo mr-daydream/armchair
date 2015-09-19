9946852717f866894c5c0d3e14a1b8ec5c4c7996
Joshua Ziggas <zoshjiggas@gmail.com>
2015-09-19 15:17:12 -0400

    js rewrite

---
+++ armchair_client.js
@@ -0,0 +1,48 @@
+// var app = require('http').createServer(handler)
+// var five = require("johnny-five");
+// var board = new five.Board();
+
+var WebSocketClient = require('websocket').client;
+
+var client = new WebSocketClient();
+
+client.on('connectFailed', function(error) {
+    console.log('Connect Error: ' + error.toString());
+});
+
+client.on('connect', function(connection) {
+    console.log('WebSocket Client Connected');
+
+    connection.on('error', function(error) {
+        console.log("Connection Error: " + error.toString());
+    });
+
+    connection.on('close', function() {
+        console.log('Connection Closed');
+    });
+
+    connection.on('message', function(message) {
+        if (message.type === 'utf8') {
+            console.log("Received: '" + message.utf8Data + "'");
+        } else {
+            console.log('Message received: ', message);
+        }
+    });
+
+    function sendNumber() {
+        if (connection.connected) {
+            var number = Math.round(Math.random() * 0xFFFFFF);
+            connection.sendUTF(number.toString());
+        }
+    }
+    sendNumber();
+});
+
+client.connect('ws://localhost:8888/ws', null, 'http://localhost:8888');
+
+// board.on("ready", function() {
+//   // Create an Led on pin 13
+//   var led = new five.Led(13);
+//   // Blink every half second
+//   led.blink(500); 
+// });
