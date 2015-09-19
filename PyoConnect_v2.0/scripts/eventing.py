c53d74f26db50131baceb50a6963da2c8ec7331c
Joshua Ziggas <zoshjiggas@gmail.com>
2015-09-19 09:18:52 -0400

    ws test

--- PyoConnect_v2.0/scripts/eventing.py
+++ PyoConnect_v2.0/scripts/eventing.py
@@ -3,14 +3,14 @@ from tornado import websocket, web, ioloop
 import json
 from threading import Timer
 
-def onPoseEdge(pose, edge):
-    print('Global onPoseEdge called.')
-    MainHandler.sendPose(pose, edge)
+test = "I am here as a placeholder for the SocketHandler instance reference"
 
 class SocketHandler(websocket.WebSocketHandler):
-    def check_origin(self, origin):
-        return True
+    def initialize(self, ref_object):
+        test = self
+        self.ref_object = ref_object
 
+    # Why do all of these methods need to pass in self
     def open(self):
         print('Socket opened.')
 
@@ -18,18 +18,43 @@ class SocketHandler(websocket.WebSocketHandler):
         print('Socket closed.')
 
     def on_message(self, message):
-        self.write_message(u"You said: " + message)
+        self.write_message("Echo!:" + message)
 
     def sendPose(self, pose, edge):
-        print('sendPose called')
-        self.write(json.dumps({ pose: pose, edge: edge }))
+        print('SocketHandler sendPose called.')
+        self.write_message(json.dumps({ "pose": pose, "edge": edge }))
 
-app = web.Application([
-    (r'/ws', SocketHandler)
-])
+class ApplicationServer():
+
+    def __init__(self):
+        self.ref_object = 1
+                                           # v What is this thing    v What is that thing
+        self.application = web.Application([(r'/ws', SocketHandler, {"ref_object" : self.ref_object})])
+        # self.socket = SocketHandler(self, '/ws')
+        # self.application = web.Application([(r'/ws', self.socket)])
+
+    def start(self):
+        self.application.listen(8888)
+
+    def onPoseEdge(self, pose, edge):
+        print('ApplicationServer onPoseEdge called.')
+        # Access the SocketHandler reference some how
+        # self.onPose(pose, edge)
+
+def onPoseEdge(app, pose, edge):
+    # Tell the app instance to fire a websocket message
+    print("test: ", test)
+    app.onPoseEdge(pose, edge)
 
 if __name__ == '__main__':
-    app.listen(8888)
+
+    app = ApplicationServer()
+    app.start()
+
+    # t = Timer(3.0, onPoseEdge(app, "test1", "test2"))
+    # t.start()
+
+    # Is this blocking Timer?
     ioloop.IOLoop.instance().start()
-    t = Timer(3.0, onPoseEdge(MainHandler, "pose", "edge"))
-    t.start()
+
+    
--- armchair.js
+++ armchair.js
@@ -34,7 +34,6 @@ client.on('connect', function(connection) {
         if (connection.connected) {
             var number = Math.round(Math.random() * 0xFFFFFF);
             connection.sendUTF(number.toString());
-            setTimeout(sendNumber, 1000);
         }
     }
     sendNumber();
